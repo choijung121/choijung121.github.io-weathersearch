f36a67f0a868f852b7432c3687c3b04f44e22028
Jung Choi <jungchoi@Jungs-MacBook-Pro.local>
2020-08-09 10:11:34 -0600

    updates in javascript

--- script.js
+++ script.js
@@ -11,31 +11,47 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
      }).then(function(json) {
        let results = "";
         results += '<div class = wrapBox>';
-        results += '<div class = box>';
-        results += '<div class = boxContent>';
-        results += '<h2 class = "cityName">Weather in ' + json.name + ", " + json.sys.country + "</h2>";
-        results += '<h2 class = "mainTemp">' + Math.round(json.main.temp) + " &deg;F</h2>"
-        results += "<p>"
-        for (let i=0; i < json.weather.length; i++) {
-          results += json.weather[i].description
-          if (i !== json.weather.length - 1)
-           results += ", "
-        }
-        results += "</p>";
-        results += '<div class = images>';
-        for (let i=0; i < json.weather.length; i++) {
-          // results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-          results += '<img src = "http://openweathermap.org/img/wn/' + json.weather[i].icon + '@2x.png"/>'Math.round(json.main.temp_max) + "&deg;F" + ' / ' + Math.round(json.main.temp_min) + "&deg;F";
-          // 'http://openweathermap.org/img/wn/10d@2x.png'
-        }
-        results += '<h2>' + Math.round(json.main.temp_max) + "&deg;F" + ' / ' + Math.round(json.main.temp_min) + "&deg;F</h2>"
-        results += '</div>';
-        results += '</div>';
-        results += '</div>';
+          results += '<div class = box>';
+            results += '<div class = boxContent>';
+
+              results += '<h2 class = "cityName"><i class="fa fa-map-marker" aria-hidden="true"></i> ' + json.name + ", " + json.sys.country + "</h2>";
+
+              //Main Temp
+              results += '<div class = images>';
+              for (let i=0; i < json.weather.length; i++) {
+                results += '<img src = "http://openweathermap.org/img/wn/' + json.weather[i].icon + '.png">' + '<span class = mainTemp>' + Math.round(json.main.temp) + " &deg;F" + '</span>';
+              }
+              results += '</div>';
+
+              //Feels like
+              results += "<p>" + "Feels like "+ json.main.feels_like + " &deg;F" + "<p>";
+
+              //Weather description
+              results += "<p><b>"
+              for (let i=0; i < json.weather.length; i++) {
+                results += json.weather[i].description
+                if (i !== json.weather.length - 1)
+                 results += ", "
+              }
+              results += "</b></p>";
+
+              //weather Min/Max Temp
+              results += '<p>' + Math.round(json.main.temp_max) + "&deg;F" + ' / ' + Math.round(json.main.temp_min) + "&deg;F" + '<p>';
+
+            results += '</div>';
+          results += '</div>';
         results += '</div>';
 
+      document.getElementById("weatherResults").innerHTML = results;
+    });
+   const hourlyUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",{state code}" + ",{country code}" + ",US&units=imperial" + "&APPID=0b9d58182e86097e433ad892d91116bd";
+   fetch(hourlyUrl)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json){
+      let forecast = "";
+      forecast += "<h2>Hourly Focecast" + "</h2>";
 
-
-        document.getElementById("weatherResults").innerHTML = results;
-   });
+      document.getElementById("forecastResults").innerHTML = forecast;
+    });
 });
