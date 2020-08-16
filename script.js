4c1b52eeb419587440977d5e6aaa58da35b81cf5
Jung Choi <jungchoi@Jungs-MacBook-Pro.local>
2020-08-16 11:07:49 -0600

    final touches

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
               //Main Temp
               results += '<div class = images>';
               for (let i=0; i < json.weather.length; i++) {
-                results += '<img src = "http://openweathermap.org/img/wn/' + json.weather[i].icon + '.png">' + '<span class = mainTemp>' + Math.round(json.main.temp) + " &deg;F" + '</span>';
+                results += '<img src = "http://openweathermap.org/img/wn/' + json.weather[i].icon + '@2x.png">' + '<span class = mainTemp>' + Math.round(json.main.temp) + " &deg;F" + '</span>';
               }
               results += '</div>';
               //Weather description
@@ -102,5 +102,22 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
         results += '</div>';
 
       document.getElementById("weatherResults").innerHTML = results;
+    });
+  // const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=0b9d58182e86097e433ad892d91116bd";
+  const url2 = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + value + "{city name}&cnt={8}" + "&APPID=0b9d58182e86097e433ad892d91116bd";
+  fetch(url2)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      let forecast = "";
+      forecast += '<h2>Hourly Forcast ' + "</h2>";
+      for (let i=0; i < json.list.length; i++) {
+        forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm a') + "</h2>";
+        forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+        forecast += "<p>Temperature: " + json.list[i].main.temp + " &deg;F</p>";
+        forecast += "<p>High of: " + json.list[i].main.temp_max + "&deg;F</p>";
+
+      }
+      document.getElementById("forecastResults").innerHTML = forecast;
     });
 });
